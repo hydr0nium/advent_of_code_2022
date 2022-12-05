c9fd25534d6d4f82fc70a2609bc878b6cec3d6ea
Sol <darkmaster2210@gmail.com>
2022-12-05 13:23:11 +0100

    Added blueprint file for exercises

---
+++ blueprint.py
@@ -0,0 +1,17 @@
+
+from typing import *
+
+def main():
+    with open("./input") as f:
+        lines = f.readlines()
+        for line in lines:
+            process(line)
+
+def process(line: str):
+    pass
+
+def remove_backslash_n(string: str):
+    return string.strip("\n")
+
+if __name__ == "__main__":
+    main()
