1b22b452a63340e23108432a1301d779b928f10e
Sol <darkmaster2210@gmail.com>
2022-12-12 21:32:20 +0100

    Added solution for day 12 of advent of code

---
+++ hill_climbing_algorithm/solution2.py
@@ -0,0 +1,100 @@
+
+from typing import *
+import dijkstra as di
+
+
+def main():
+    with open("./input.txt") as f:
+        counter = 0
+        graph: di.Graph = di.Graph()
+        start = ""
+        end = ""
+        lines = list(map(lambda x: x.strip("\n"),f.readlines()))
+        grid = [] # y, x
+        a_nodes = []
+        for line in lines:
+            row = []
+            for letter in line:
+                if letter == "S":
+                    start = "S" + str(counter)
+                if letter == "E":
+                    end = "E" + str(counter)
+                if letter == "a":
+                    a_nodes.append("a" + str(counter))
+                row.append(letter + str(counter))
+                counter +=1
+            grid.append(row)
+        for row in grid:
+            graph = connect_row(graph, row)
+        for i in range(len(grid)-1):
+            row_now = grid[i]
+            row_next = grid[i+1]
+            graph = connect_rows(graph, row_now, row_next)
+        lowest = counter
+        for node in a_nodes:
+            try:
+                dijkstra = di.DijkstraSPF(graph, node)
+                path = dijkstra.get_path(end)
+                path_len = len(path)-1
+                if path_len < lowest:
+                    lowest = path_len
+            except:
+                pass
+        print(lowest)
+
+def connect_row(graph: di.Graph, row):
+    for i in range(len(row)-1):
+        node_a = row[i]
+        node_b = row[i+1]
+        letter_a = node_a[0]
+        letter_b = node_b[0]
+        if letter_a == "S":
+            letter_a = "a"
+        if letter_b == "S":
+            letter_b = "a"
+        if letter_a == "E":
+            letter_a = "z"
+        if letter_b == "E":
+            letter_b = "z"
+        val_a = ord(letter_a)
+        val_b = ord(letter_b)
+        weight = 1
+        if val_a >= val_b:
+            graph.add_edge(node_a, node_b, weight)
+        if val_b >= val_a:
+            graph.add_edge(node_b, node_a, weight)
+        if abs(val_a-val_b)<=1:
+            graph.add_edge(node_a, node_b, weight)
+            graph.add_edge(node_b, node_a, weight)
+    return graph
+
+def connect_rows(graph: di.Graph, row_now, row_next):
+    for i in range(len(row_now)):
+        node_a = row_now[i]
+        node_b = row_next[i]
+        letter_a = node_a[0]
+        letter_b = node_b[0]
+        if letter_a == "S":
+            letter_a = "a"
+        if letter_b == "S":
+            letter_b = "a"
+        if letter_a == "E":
+            letter_a = "z"
+        if letter_b == "E":
+            letter_b = "z"
+        val_a = ord(letter_a)
+        val_b = ord(letter_b)
+        weight = 1
+        if val_a >= val_b:
+            graph.add_edge(node_a, node_b, weight)
+        if val_b >= val_a:
+            graph.add_edge(node_b, node_a, weight)
+        if abs(val_a-val_b)<=1:
+            graph.add_edge(node_a, node_b, weight)
+            graph.add_edge(node_b, node_a, weight)
+        
+    return graph
+
+
+if __name__ == "__main__":
+    main()
